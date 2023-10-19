ea4ca717801b4d7e8b9e5bbb11d4cf0510eb23e0
TIP <seanseol@naver.com>
2023-10-19 12:42:05 +0900

    Drill10 제출 (2018180022방영규)

---
+++ grass.py
@@ -0,0 +1,14 @@
+from pico2d import load_image
+
+
+class Grass:
+    def __init__(self,x,y):
+        self.image = load_image('grass.png')
+        self.x = x
+        self.y = y
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+    def update(self):
+        pass
